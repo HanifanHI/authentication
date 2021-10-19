b80cc9c55f16491116ed6d7963bdf540272b72f6
HanifanHI <hanifanisnanto@gmail.com>
2021-10-19 12:43:04 +0700

    first commit

---
+++ lib/models/product.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// * MODEL PRODUCT
+class Product {
+  String id, title, price;
+  DateTime createdAt, updatedAt;
+
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.price,
+    @required this.createdAt,
+    this.updatedAt,
+  });
+}
